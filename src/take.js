2641a50eec042ab39cfe96d7fbcef7a406151a63
Nicklas Nygren <hello@nickl.as>
2016-04-20 16:18:56 +0200

    Base take off of slice

--- src/take.js
+++ src/take.js
@@ -1,12 +1,6 @@
 import cu from 'auto-curry';
 import slice from './slice';
 
-export default cu(function * take(len, iterable) {
-  let n = 0;
-  for (const item of iterable) {
-    if (++n > len) {
-      break;
-    }
-    yield item;
-  }
+export default cu(function take(len, iterable) {
+  return slice(0, len, iterable);
 });
