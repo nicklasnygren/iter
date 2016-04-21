9eb45018cd9bcc9b5ee7f5bac6d42ee629fb4ea4
Nicklas Nygren <hello@nickl.as>
2016-04-21 15:15:50 +0200

    Use getIterator in compose

--- src/compose.js
+++ src/compose.js
@@ -5,11 +5,11 @@ export default function compose(...iterables) {
   const iterator = function * (iterable) {
     yield * iterables
       .reverse()
-      .reduce((iterator, iterable) => iterable(iterator), iterable)
+      .reduce((prev, cur) => cur(prev), iterable)
   }
 
   if (isIterable(iterables[iterables.length - 1])) {
-    return iterator(iterables.pop());
+    return iterator(getIterator(iterables.pop()));
   }
   else {
     return iterator;
