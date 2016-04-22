77c441e7203cd58c233c2a276f5863c141785302
Nicklas Nygren <hello@nickl.as>
2016-04-22 11:47:33 +0200

    Add fizzbuzz test

--- test/compose.js
+++ test/compose.js
@@ -1,5 +1,5 @@
 import test from 'tape';
-import { compose, filter, map, intRange, getArray } from '../src';
+import { compose, filter, map, intRange, getArray, zipWith, take, compact } from '../src';
 
 test('compose', t => {
   
@@ -33,6 +33,19 @@ test('compose', t => {
     ),
     [0, 0, 0],
     `compose returns function if leftmost argument not iterable`
+  );
+
+  const _fizzBuzz = compose(
+    map(n => n % 3 === 0 ? 'fizz' : n),
+    map(n => n % 5 === 0 ? 'buzz' : n),
+    map(n => n % 5 === 0 && n % 3 === 0 ? 'fizzbuzz' : n),
+    intRange(1, Infinity)
+  );
+
+  t.deepEqual(
+    getArray(take(15, _fizzBuzz)),
+    [1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8 , 'fizz', 'buzz', 11, 'fizz', 13, 14, 'fizzbuzz'],
+    `Composition can provide interviewers with a much needed fizzBuzz algorithm`
   );
 
   t.end();
